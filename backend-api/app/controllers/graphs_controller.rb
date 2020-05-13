549831c4d7da02895300b6c6eb96414427e2154f
stevenks17 <stevenks17@gmail.com>
2020-05-13 14:47:10 -0400

    added cors gem and settings

--- backend-api/app/controllers/graphs_controller.rb
+++ backend-api/app/controllers/graphs_controller.rb
@@ -1,2 +1,33 @@
 class GraphsController < ApplicationController
+    def index
+        @graphs = Graph.all
+        render json: @graphs.to_json(:except => [:updated_at, :created_at])
+    end
+
+    def show
+        @graph = Graph.find(params[:id])
+        render json: @graphs.to_json(:except => [:updated_at, :created_at])
+    end
+
+    def create
+        @graph = Graph.new(graph_params)
+        if @graph.save
+            render json: @graphs.to_json(:except => [:updated_at, :created_at])
+        else
+            render json: {error: "Graph could not be saved to the matrix. Please see Neo for assistance."}
+        end
+    end
+
+    def destroy
+        @graph = Graph.find(params[:id])
+        @graph.destroy
+        render json: {alert: "Graph Deleted"}
+    end
+
+    private
+
+    def graph_params
+        params.require(:graph).permit(:name, :info_url, :screenshot_url)
+    end
+
 end
