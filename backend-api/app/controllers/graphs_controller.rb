7aa25635cd6e58724a63a33a9d485ea9c9e256b1
github username <github email address>
2020-05-13 17:57:39 -0400

    added controller logic

--- backend-api/app/controllers/graphs_controller.rb
+++ backend-api/app/controllers/graphs_controller.rb
@@ -1,25 +1,22 @@
 class GraphsController < ApplicationController
+    before_action :set_params, only: [:show, :destroy]
+
     def index
         @graphs = Graph.all
         render json: @graphs
     end
 
-    def show
-        @graph = Graph.find(params[:id])
-        render json: @graphs
+    def show    
+        render json: @graphs, status: 200
     end
 
     def create
-        @graph = Graph.new(graph_params)
-        if @graph.save
-            render json: @graphs
-        else
-            render json: {error: "Graph could not be saved to the matrix. Please see Neo for assistance."}
-        end
+        @graph = Graph.create(graph_params)
+        render json: @graph, status: 200
+
     end
 
     def destroy
-        @graph = Graph.find(params[:id])
         @graph.destroy
         render json: {alert: "Graph Deleted"}
     end
@@ -30,4 +27,7 @@ class GraphsController < ApplicationController
         params.require(:graph).permit(:name, :info_url, :screenshot_url)
     end
 
+    def set_params
+        @graph = Graph.find(params[:id])
+    end
 end
