538c7d31c7df322c7522c87f621e8eb04798af69
github username <github email address>
2020-05-13 18:10:55 -0400

    duplicated notes controller and adjusted variables in Questions Controller.

--- backend-api/app/controllers/questions_controller.rb
+++ backend-api/app/controllers/questions_controller.rb
@@ -1,2 +1,36 @@
 class QuestionsController < ApplicationController
+    before_action :set_params only: [:show, :destroy]
+
+    def index
+        @questions = @graphs.notes
+        render json: @questions, status: 200
+    end
+
+    def show
+        render json: @question
+    end
+
+    def create
+        @question = @graph.questions.new(question_params)
+        if @question.save
+            render json: @question
+        else
+            render json: {error: " Question was not saved to database. Please check with Neo."}
+        end
+    end
+
+    def destroy
+        @question.destroy
+        render json: {alert:"Question deleted."}
+    end
+
+    private
+
+    def question_params
+        params.require(:note).permit(:content, :graph_id)
+    end
+
+    def set_params
+        @graph = Graph.find(params[:graph_id])
+    end
 end
