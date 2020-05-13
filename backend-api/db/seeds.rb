3c58d8830084c6993f39cd1ec82d4206f3f00950
github username <github email address>
2020-05-13 17:55:22 -0400

    edited seed data

--- backend-api/db/seeds.rb
+++ backend-api/db/seeds.rb
@@ -5,12 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Graph.destroy_all
+Note.destroy_all
+
+puts "Entering data into the MATRIX !!!"
+
 graph1 = {
     name: "National Syndromic Surveillance Program (NSSP): Emergency Department Visits Percentage of Visits for COVID-19-Like Illness  September 29, 2019 - April 4, 2020 Data as of April 9, 2020",
     info_url: "https://www.cdc.gov/coronavirus/2019-ncov/covid-data/covidview/04102020/nssp-regions.html",
     screenshot_url: "https://www.cdc.gov/coronavirus/2019-ncov/covid-data/covidview/04102020/covid-like-illness.png"
 }
 
-graph1 = Graph.new(graph1)
+graph1 = Graph.create(graph1)
 
-graph1.save
+
+note1 = Note.create(content: "These COVID19 like symptoms appear to be very similar to the influenza.", graph_id: 1)
+
+
+question1 = Question.create(content: "Is the spike in cases related to temperature when referring to COVID19 ?", graph_id: 1)
+
