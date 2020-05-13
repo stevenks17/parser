1f64418e10b812f6e86902394904121ba944cfd1
github username <github email address>
2020-05-13 17:57:21 -0400

    Nested notes/question route

--- backend-api/config/routes.rb
+++ backend-api/config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  resources :questions
-  resources :notes
-  resources :graphs
+
+  resources :graphs do 
+    resources :questions
+    resources :notes
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
