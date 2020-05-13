9ab151739810f4b0738ac9c9b9bce597340a47ad
stevenks17 <stevenks17@gmail.com>
2020-05-13 14:37:10 -0400

    added associations/validations

--- backend-api/app/models/graph.rb
+++ backend-api/app/models/graph.rb
@@ -2,5 +2,7 @@ class Graph < ApplicationRecord
     has_many :notes
     has_many :questions
 
-    
+    validates :name, presence: true
+    validates :info_url, presence: true
+    validates :screenshot_url, presence: true
 end
