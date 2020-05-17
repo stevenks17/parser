81a29f78fd8601588a222f173eb11888990ea2fd
stevenks17 <stevenks17@gmail.com>
2020-05-17 12:08:32 -0400

    Update addDataSource.js
    pulled from WGN-App

--- frontend-react-redux/src/actions/addDataSource.js
+++ frontend-react-redux/src/actions/addDataSource.js
@@ -3,13 +3,29 @@
 export const addDataSource = (dataSource) => {
 
     return dispatch => {
-        dispatch({ type: 'START_ADDING_DATQA_SOURCE_REQUEST'})
-
+        dispatch({ type: 'START_ADDING_DATA_SOURCE_REQUEST' })
+        
         let configObj = {
-            method: "POST", 
+            method: "POST",
             headers: {
-                ""
-            }
-        }
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            },
+            body: JSON.stringify(dataSource)
+        };
+
+        fetch(`http://localhost:3000/charts`, configObj)
+        .then(function(response) {
+            return response.json()
+        })
+        // dispatches action.type and data object to charts reducer after recieving json response from front end
+        .then(data => {
+        console.log(data)
+            dispatch({ type: 'ADD_DATA_SOURCE', data })})
+
+        .catch(function(error) {
+            alert("ERROR! Please Try Again");
+            console.log(error.message);
+        });
     }
 }
