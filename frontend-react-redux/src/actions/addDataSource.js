b6003da5f9b08a3d241d545114be6a6e5779472b
github username <github email address>
2020-05-17 12:07:23 -0400

    setting up  method type and response for adding dataSource

---
+++ frontend-react-redux/src/actions/addDataSource.js
@@ -0,0 +1,15 @@
+// takes in argument of an object with form data(name and urls)
+
+export const addDataSource = (dataSource) => {
+
+    return dispatch => {
+        dispatch({ type: 'START_ADDING_DATQA_SOURCE_REQUEST'})
+
+        let configObj = {
+            method: "POST", 
+            headers: {
+                ""
+            }
+        }
+    }
+}
