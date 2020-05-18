14b6dbd897ca3455f08a44129e70fb09797a6cb7
github username <github email address>
2020-05-18 18:49:30 -0400

    added render logic for questions

---
+++ frontend-react-redux/src/components/Questions.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+
+import Question from '../components/Question'
+//future action
+import { deleteQuestion } from '../actions/deleteSpeculations'
+// connect to redux store
+import { connect } from 'react-redux'
+
+
+class Questions extends Component {
+    render() {
+        return (
+            <div>
+                {(note => <Question question={note} graph={this.props.graph} deleteQuestion={this.props.deleteQuestion}/>)}
+            </div>
+        )
+    }
+}
+
+export default connect(null, {deleteQuestion}(Questions))
