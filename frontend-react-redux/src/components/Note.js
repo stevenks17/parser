7841473281dc2e0b4c5fc6d85f77283529e43f54
github username <github email address>
2020-05-18 15:27:09 -0400

    re-installed bootstrap setup note component

---
+++ frontend-react-redux/src/components/Note.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react'
+
+
+import Card from 'react-bootstrap/Card'
+import Button from 'react-bootstrap/Button'
+
+class Note extends Component {
+    render() {
+        // conditional to render content using buttons if already saved
+        if (this.props.graph) {
+
+            return (
+                <Card className="text-center">
+                    <Card.body>
+                    <Card.Header> Add Observation Notes</Card.Header>
+                        <Card.Text>
+                            {this.props.note.content}
+                        </Card.Text>
+                    </Card.body>
+
+
+                </Card>
+            )
+        // renders only when content is not yet saved to db
+        } else {
+            return (
+                <Card className="text-center">
+                    <Card.Body>
+                        <Card.Header> Add Observation Note </Card.Header>
+                        <Card.Title>
+                            {this.props.note.content}
+                        </Card.Title>
+                    </Card.Body>
+                </Card>
+
+            )
+        }
+    }
+}
+
+
+export default Note
