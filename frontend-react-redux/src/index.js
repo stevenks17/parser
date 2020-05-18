759883907ead8f2f1334f0f33bf7d7e4a17a2ac6
stevenks17 <stevenks17@gmail.com>
2020-05-18 15:27:09 -0400

    re-installed bootstrap setup note component

--- frontend-react-redux/src/index.js
+++ frontend-react-redux/src/index.js
@@ -4,7 +4,6 @@ import './index.css';
 import App from './containers/App';
 
 import { Provider } from 'react-redux';
-import * as serviceWorker from './serviceWorker';
 import {createStore, applyMiddleware, compose} from 'redux'
 import thunk from 'redux-thunk;'
 import 'bootstrap/dist/css/bootstrap.min.css'
@@ -25,7 +24,4 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
