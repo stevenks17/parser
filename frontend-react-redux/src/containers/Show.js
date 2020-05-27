63e3e43e45f808df8c42eb843aefeddd63398ea2
stevenks17 <stevenks17@gmail.com>
2020-05-27 13:29:50 -0400

    fixed comparison for filter function in show

--- frontend-react-redux/src/containers/Show.js
+++ frontend-react-redux/src/containers/Show.js
@@ -22,9 +22,10 @@ class Show extends Component {
 
     render() {
     // variables for objects
-    const graph = this.props.graphs.find(graph => {return graph.id === parseInt(this.props.match.params.id)})
-    const notes = this.props.notes.filter(note => note.graph_id === graph.id)
-    const questions = this.props.questions.filter(question => question.graph_id === graph.id)
+    const graph = this.props.graphs.find(graph => { return graph.id === parseInt(this.props.match.params.id)})
+    const notes = this.props.notes.filter(note => note.graph.id === graph.id)
+    const questions = this.props.questions.filter(question => question.graph.id === graph.id)
+
 
     // if graph exists it loads all corresponding notes and questions with it
     if (graph) {
