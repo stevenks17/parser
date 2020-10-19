2e3844969933a424492d1a58246fba42270fd8e1
stevenks17 <stevenks17@gmail.com>
2020-10-19 18:06:23 -0400

    Answers Migration created

--- backend-api/db/schema.rb
+++ backend-api/db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_13_183033) do
+ActiveRecord::Schema.define(version: 2020_05_26_172118) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "answers", force: :cascade do |t|
+    t.string "content"
+    t.bigint "question_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["question_id"], name: "index_answers_on_question_id"
+  end
 
   create_table "graphs", force: :cascade do |t|
     t.string "name"
@@ -39,6 +47,7 @@ ActiveRecord::Schema.define(version: 2020_05_13_183033) do
     t.index ["graph_id"], name: "index_questions_on_graph_id"
   end
 
+  add_foreign_key "answers", "questions"
   add_foreign_key "notes", "graphs"
   add_foreign_key "questions", "graphs"
 end
